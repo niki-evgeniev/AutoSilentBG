899ddf9c3425b3ca8d9bf1a9aa3421ef04d5190b
Nikolay Ivanov <niki.evgeniev@gmail.com>
2026-06-29 01:39:19 +0300

    Update user controller

--- src/main/java/nevg/nirton/Controller/UserController.java
+++ src/main/java/nevg/nirton/Controller/UserController.java
@@ -1,12 +1,28 @@
 package nevg.nirton.Controller;
 
 
+import jakarta.validation.Valid;
+import nevg.nirton.Models.Dto.UserRegistrationDto;
+import nevg.nirton.Service.EmailAlreadyExistsException;
+import nevg.nirton.Service.UserRegistrationService;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.util.Objects;
 
 @Controller
 public class UserController {
+
+    private final UserRegistrationService userRegistrationService;
+
+    public UserController(UserRegistrationService userRegistrationService) {
+        this.userRegistrationService = userRegistrationService;
+    }
 
     @GetMapping("/user/sign_in")
     public ModelAndView login(){
@@ -14,7 +30,37 @@ public class UserController {
     }
 
     @GetMapping("/user/sign_up")
-    public ModelAndView register(){
-        return new ModelAndView("register");
+    public ModelAndView register() {
+        return registrationView(new UserRegistrationDto());
+    }
+
+    @PostMapping("/user/sign_up")
+    public ModelAndView register(@Valid @ModelAttribute("registration") UserRegistrationDto registration,
+                                 BindingResult bindingResult,
+                                 RedirectAttributes redirectAttributes) {
+        if (!Objects.equals(registration.getPassword(), registration.getConfirmPassword())) {
+            bindingResult.rejectValue("confirmPassword", "password.mismatch", "Паролите не съвпадат.");
+        }
+
+        if (bindingResult.hasErrors()) {
+            return registrationView(registration);
+        }
+
+        try {
+            userRegistrationService.register(registration);
+        } catch (EmailAlreadyExistsException exception) {
+            bindingResult.rejectValue("email", "email.exists", exception.getMessage());
+            return registrationView(registration);
+        }
+
+        redirectAttributes.addFlashAttribute("registrationSuccess",
+                "Регистрацията е успешна. Вече можете да влезете в профила си.");
+        return new ModelAndView("redirect:/user/sign_in");
+    }
+
+    private ModelAndView registrationView(UserRegistrationDto registration) {
+        ModelAndView modelAndView = new ModelAndView("register");
+        modelAndView.addObject("registration", registration);
+        return modelAndView;
     }
 }
