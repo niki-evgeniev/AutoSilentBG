17ee969287e9cfbc365433e26bcd15f9f59bc2c0
Nikolay Ivanov <niki.evgeniev@gmail.com>
2026-06-29 01:40:10 +0300

    refactoring

---
+++ src/main/java/nevg/nirton/Models/Security/ShopUserDetails.java
@@ -0,0 +1,22 @@
+package nevg.nirton.Models.Security;
+
+import org.springframework.security.core.GrantedAuthority;
+
+import java.util.Collection;
+
+public final class ShopUserDetails extends org.springframework.security.core.userdetails.User {
+
+    private final String firstName;
+
+    public ShopUserDetails(String email,
+                           String password,
+                           String firstName,
+                           Collection<? extends GrantedAuthority> authorities) {
+        super(email, password, authorities);
+        this.firstName = firstName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+}
