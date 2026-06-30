b7582e93ba3dd885efd305cc26345cdc6496f19c
Nikolay Ivanov <niki.evgeniev@gmail.com>
2026-07-01 01:32:36 +0300

    Add i18n config

---
+++ src/main/java/nevg/nirton/AppConfiguration/InternationalizationConfiguration.java
@@ -0,0 +1,47 @@
+package nevg.nirton.AppConfiguration;
+
+import org.jspecify.annotations.NonNull;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.CookieLocaleResolver;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+
+import java.time.Duration;
+import java.util.Locale;
+
+@Configuration
+public class InternationalizationConfiguration implements WebMvcConfigurer {
+
+    public static final Locale BULGARIAN = Locale.forLanguageTag("bg");
+
+    @Bean
+    public LocaleResolver localeResolver() {
+        CookieLocaleResolver resolver = new CookieLocaleResolver("NIRTON_LOCALE");
+        resolver.setCookieMaxAge(Duration.ofDays(365));
+        resolver.setCookiePath("/");
+        resolver.setCookieHttpOnly(true);
+        resolver.setCookieSameSite("Lax");
+        resolver.setLanguageTagCompliant(true);
+        resolver.setDefaultLocaleFunction(request ->
+                "bg".equalsIgnoreCase(request.getLocale().getLanguage())
+                        ? BULGARIAN
+                        : Locale.ENGLISH);
+        return resolver;
+    }
+
+    @Bean
+    public LocaleChangeInterceptor localeChangeInterceptor() {
+        LocaleChangeInterceptor interceptor = new LocaleChangeInterceptor();
+        interceptor.setParamName("lang");
+        interceptor.setIgnoreInvalidLocale(false);
+        return interceptor;
+    }
+
+    @Override
+    public void addInterceptors(@NonNull InterceptorRegistry registry) {
+        registry.addInterceptor(localeChangeInterceptor());
+    }
+}
